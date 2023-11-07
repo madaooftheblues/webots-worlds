f796c03cd5f5f8e3d1cc5a976cb106e51032603a
madaooftheblues <azlanrasool@gmail.com>
2023-11-08 02:23:00 +0500

    Launch server on supervisor controller

--- my_project/controllers/supervisor_cam_controller/supervisor_cam_controller.py
+++ my_project/controllers/supervisor_cam_controller/supervisor_cam_controller.py
@@ -3,12 +3,47 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Supervisor, Camera
+import threading
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import uvicorn
 
 # create the Robot instance.
 robot = Supervisor()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# Get the time step of the current world (simulation)
+time_step = int(robot.getBasicTimeStep())
+
+# Create a FastAPI app
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/webots/play")
+async def play_simulation():
+    robot.simulationSetMode(robot.SIMULATION_MODE_FAST)
+    robot.step(time_step)
+    return {"message" : "Simulation started"}
+@app.post("/webots/pause")
+async def pause_simulation():
+    robot.simulationSetMode(robot.SIMULATION_MODE_PAUSE)
+    robot.step(0)
+    return {"message" : "Simulation stopped"}
+
+# Function to start the FastAPI server
+def start_fastapi_server():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# Start the FastAPI server in a separate thread
+fastapi_server_thread = threading.Thread(target=start_fastapi_server)
+fastapi_server_thread.start()
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -16,10 +51,10 @@ timestep = int(robot.getBasicTimeStep())
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 camera = robot.getDevice("camera")
-camera.enable(timestep)
+camera.enable(time_step)
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+while robot.step(time_step) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
